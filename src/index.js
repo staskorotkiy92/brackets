fec8a1d21d1a06776a80f580b28fd92526187cfb
staskorotkiy92 <staskorotkiy92@gmail.com>
2020-03-23 04:33:29 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2) return false;
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        if (stack[stack.length - 1] === str[i]) {
+            stack.length--;
+        }
+        else {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (bracketsConfig[j][0] === str[i])
+                    stack.push(bracketsConfig[j][1]);
+            }
+        }
+    }
+    if (!stack.length) return true;
+    return false;
+};
+
+
+
+
+
+
+
+
